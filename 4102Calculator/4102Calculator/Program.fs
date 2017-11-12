ef6ca5f4e0fcec91bc302e8ff046901926be37d9
mcropper <mcropper@uncc.edu>
2017-11-12 16:06:31 -0500

    commit

--- 4102Calculator/4102Calculator/Program.fs
+++ 4102Calculator/4102Calculator/Program.fs
@@ -40,39 +40,5 @@ type CalculatorServices = {
     initState: InitState 
     }
 
-//type Calculate = CalculatorInput * CalculatorState -> CalculatorState 
-//and CalculatorState = {
-//    display: CalculatorDisplay
-//    }
-//and CalculatorDisplay = string
-//and CalculatorInput = 
-//    | Digit of CalculatorDigit
-//    | Add | Subtract | Multiply | Divide
-//    | Equals | Clear
 
-//type UpdateDisplayFromDigit = CalculatorDigit * CalculatorDisplay -> CalculatorDisplay
-//and CalculatorDigit = 
-//    | Zero | One | Two | Three | Four 
-//    | Five | Six | Seven | Eight | Nine
-//    | DecimalSeparator
-
-//let convertInputToDigit (input: CalculatorInput) =
-//    match input with 
-//        | CalculatorInput.Zero -> CalculatorDigit.Zero
-//        | CalculatorInput.One -> CalculatorDigit.One
-//        | CalculatorInput.Two -> CalculatorDigit.Two
-//        | CalculatorInput.Three -> CalculatorDigit.Three
-//        | CalculatorInput.Four -> CalculatorDigit.Four
-//        | CalculatorInput.Five -> CalculatorDigit.Five
-//        | CalculatorInput.Six -> CalculatorDigit.Six
-//        | CalculatorInput.Seven -> CalculatorDigit.Seven
-//        | CalculatorInput.Eight -> CalculatorDigit.Eight
-//        | CalculatorInput.Nine -> CalculatorDigit.Nine
-//        //| CalculatorInput.Add -> ???
-//        //| CalculatorInput.Clear -> ???
-
-//let demo() = 
-//    printfn "test commit"
-
-//demo()
 Console.ReadKey() |>ignore 
